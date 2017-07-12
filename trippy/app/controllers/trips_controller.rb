e08f98242c8e39890e129f67e4502340bce63c41
Gary Berg <gberg@Garys-MacBook-Pro.local>
2017-07-11 18:15:01 -0700

    trips query progress

--- trippy/app/controllers/trips_controller.rb
+++ trippy/app/controllers/trips_controller.rb
@@ -3,5 +3,26 @@ class TripsController < ApplicationController
   end
 
   def show
+    amount = params[:budget].to_i / 3
+    dest = params[:city]
+
+    set_transportation(amount, dest)
+    set_hospitality(amount)
+    set_activities(amount)
+
+  end
+
+  private 
+
+  def set_transportation(amount, dest)
+    @transportation = Transportation.where(["cost <= :cost and end = :end", { cost: amount, end: dest }])
+  end
+  
+  def set_hospitality(amount)
+    @hospitality = Transportation.where("cost <= ?", amount)
+  end
+  
+  def set_activities(amount)
+    @activity = Transportation.where("cost <= ?", amount)
   end
 end
