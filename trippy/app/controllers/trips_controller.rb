8471586b83f6d8b63c6d523099c2d6b4ad218395
Gary Berg <gberg@Garys-MacBook-Pro.local>
2017-07-12 09:44:52 -0700

    Fix activities bug

--- trippy/app/controllers/trips_controller.rb
+++ trippy/app/controllers/trips_controller.rb
@@ -6,8 +6,8 @@ class TripsController < ApplicationController
     amount = params[:budget].to_i
     dest = params[:city]
     start = params[:start]
-    duration = params[:duration]
-    
+    duration = params[:duration].to_i
+
     set_cheap(amount, dest, start)
     amount = params[:budget].to_i
     set_expensive(amount, dest, start)
@@ -19,84 +19,99 @@ class TripsController < ApplicationController
 
   def set_cheap(amount, dest, start)
     set_transportation(amount, dest, start)
-    unless @transportion_cheap.nil? || @transportion_cheap[0].nil? 
-      amount = amount - @transportation_cheap[0].cost 
+    unless @transportation_cheap.nil? || @transportation_cheap[0].nil?
+      amount = amount - @transportation_cheap[0].cost
+      puts "amount 1 = #{amount}"
     end
     set_hospitality(amount, dest)
-    unless @hospilatity_cheap.nil? || @hospitality_cheap[0].nil? 
+    unless @hospilatity_cheap.nil? || @hospitality_cheap[0].nil?
       amount = amount - @hospitality_cheap[0].cost
+      puts "amount 2 = #{amount}"
     end
+    puts "amount final = #{amount}"
     set_activities_cheap(amount, dest)
-  end 
+  end
 
   def set_expensive(amount, dest, start)
     set_transportation_high(amount, dest, start)
-    unless @transportation_high.nil? || @transportation_high[0].nil? 
+    unless @transportation_high.nil? || @transportation_high[0].nil?
       amount = amount - @transportation_high[0].cost
     end
     set_hospitality_high(amount, dest)
-    unless @hospitality_high.nil? || @hospitality_high[0].nil? 
+    unless @hospitality_high.nil? || @hospitality_high[0].nil?
       amount = amount - @hospitality_high[0].cost
     end
     set_activities_high(amount, dest)
-  end 
-  
+  end
+
   def set_random(amount, dest, start)
     set_transportation_rand(amount, dest, start)
-    unless @transportation_rand.nil? || @transportation_rand[0].nil? 
+    unless @transportation_rand.nil? || @transportation_rand[0].nil?
       amount = amount - @transportation_rand[0].cost
     end
     set_hospitality_rand(amount, dest)
-    unless @hospitality_rand.nil? || @hospitality_rand[0].nil? 
+    unless @hospitality_rand.nil? || @hospitality_rand[0].nil?
       amount = amount - @hospitality_rand[0].cost
     end
-    set_activities(amount, dest)
-  end 
+    set_activities_rand(amount, dest)
+  end
 
 
   def set_transportation(amount, dest, start)
     @transportation_cheap = Transportation.limit(1).order("cost").where("cost <= '%s' and dest = '%s' and start = '%s'" , amount, dest, start)
   end
-  
+
   def set_hospitality(amount, dest)
     daily_cost = amount / params[:duration].to_i
     @hospitality_cheap = Hospitality.limit(1).order("cost").where("cost <= '%s' and city = '%s'" , daily_cost, dest)
   end
-  
+
   def set_activities_cheap(amount, dest)
-    @activities_cheap = Activities.limit(2 * params[:duration].to_i).order("cost").where("cost <= '%s' and city = '%s'", amount, dest)
+    total = 0
+    @activities_cheap = Array.new
+    Activities.limit(2 * params[:duration].to_i).order("cost").where("cost <= '%s' and city = '%s'", amount, dest).each do |act|
+      break if ((total += act.cost) > amount)
+      total += act.cost
+      @activities_cheap << act
+    end
   end
-  
+
   def set_transportation_high(amount, dest, start)
     @transportation_high = Transportation.limit(1).order("cost DESC").where("cost <= '%s' and dest = '%s' and start = '%s'" , amount, dest, start)
   end
-  
+
   def set_hospitality_high(amount, dest)
     daily_cost = amount / params[:duration].to_i
     @hospitality_high = Hospitality.limit(1).order("cost DESC").where("cost <= '%s' and city = '%s'" , daily_cost, dest)
   end
-  
+
   def set_activities_high(amount, dest)
-    @activities_high = Activities.limit(2 * params[:duration].to_i).order("cost DESC").where("cost <= '%s' and city = '%s'", amount, dest)
+    total = 0
+    @activities_high = Array.new
+    Activities.limit(2 * params[:duration].to_i).order("cost DESC").where("cost <= '%s' and city = '%s'", amount, dest).each do |act|
+      break if ((total += act.cost) > amount)
+      total += act.cost
+      @activities_high << act
+    end
   end
 
   def set_transportation_rand(amount, dest, start)
     @transportation_rand = Transportation.limit(1).where("cost <= '%s' and dest = '%s' and start = '%s'" , amount, dest, start)
   end
-  
+
   def set_hospitality_rand(amount, dest)
     daily_cost = amount / params[:duration].to_i
     @hospitality_rand = Hospitality.limit(1).where("cost <= '%s' and city = '%s'" , daily_cost, dest)
   end
-  
-  def set_activities(amount, dest)
+
+  def set_activities_rand(amount, dest)
     total = 0
     @activities_rand = Array.new
-    Activities.limit(2 * params[:duration].to_i).where("cost <= '%s' and city = '%s'", amount, dest).each do |act| 
+    Activities.limit(2 * params[:duration].to_i).where("cost <= '%s' and city = '%s'", amount, dest).each do |act|
       break if ((total += act.cost) > amount)
       total += act.cost
       @activities_rand << act
     end
   end
-  
+
 end
